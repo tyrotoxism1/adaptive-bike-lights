5a7fde8e3790cec41f9d81050660a9bf0e652068
tyro <jomomojo55@gmail.com>
2022-02-03 11:39:20 -0800

    leds now turn on(brake) when accelerometer reading is negative. Turn signals and all that work as well with braking

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 /** Functions **/
 float convertRawGyro(int gRaw);
 float convertRawAcc(int aRaw);
-int get_general_acc(int x, int y);
+int get_xy_magn(int x, int y);
 //takes in array, then returns the average
 int get_ma(int* arr);
 //takes in arr, shifts each value "left" discarding [0] index, and putting new val at index 20
@@ -15,7 +15,7 @@ void update_arr(int* arr,int new_val);
 
 /** Global Vars **/
 //defines the arr size of the moving avg for acc and rot if needed
-const int avg_arr_size=1;
+const int avg_arr_size=5;
 //used to keep track of time similiar to timer
 int long last_millis=0;
 int long curr_millis=0;
@@ -43,8 +43,13 @@ bool blink_left=false;
 int const led_right_pin=6;
 bool led_right_val=false;
 bool blink_right=false;
+//vars to attempt to keep track of general motion
+bool moving_forward=false;
+bool braking=false;
 
 //keep track of velocity by adding acceleration values above threshold
+//idea was to try and sum up acceleration points when motion happens, however doesn't work since valeus don't exactly
+//add to 0, so would be able to really tell if stopped or how fast person is going.
 int velocityX=0;
 int velocityY=0;
 
@@ -89,13 +94,7 @@ void loop() {
   //read button values
   btn_left_val=digitalRead(btn_left_pin);
   btn_right_val=digitalRead(btn_right_pin);
-  //priont some stuff
-  Serial.print("button val(R L): \t");
-  Serial.print(btn_right_val);
-  Serial.print("\t");
-  Serial.print(btn_left_val);
-  Serial.println();
-
+  
   //if right btn pressed(active low), blink right led
   if(btn_right_val==0){
     //flip blink val to turn toggle essentially
@@ -131,29 +130,32 @@ void loop() {
     digitalWrite(led_left_pin,(led_left_val?255:0));
     led_left_val=(!led_left_val);
   }
+  /*
+  //timer used to turn off turn signal after 5 seconds, doesnt quite work atm
   if((curr_millis-last_millis>5000) && (blink_right||blink_left)){ //if blinking turn signal is on for certain time, reset to default
     Serial.println("Turn signal reset");
     blink_left=false;
     blink_right=false;
     last_millis=curr_millis;
   }
+  */
 
 
   //add new val to arr of ma for X Y and Z arrs
   update_arr(acc_maX, (convertRawAcc(BMI160.getAccelerationX())) );
-  //accX_ma=get_ma(acc_maX);
-  accX_ma=convertRawAcc(BMI160.getAccelerationX());
+  accX_ma=get_ma(acc_maX);
+  //accX_ma=convertRawAcc(BMI160.getAccelerationX());
 
   update_arr(acc_maY, (convertRawAcc(BMI160.getAccelerationY())) );
-  //accY_ma=get_ma(acc_maY);
-  accY_ma=convertRawAcc(BMI160.getAccelerationY());
+  accY_ma=get_ma(acc_maY);
+  //accY_ma=convertRawAcc(BMI160.getAccelerationY());
 
   update_arr(acc_maZ, (convertRawAcc(BMI160.getAccelerationZ())) );
-  //accZ_ma=get_ma(acc_maZ);
-  accZ_ma=convertRawAcc(BMI160.getAccelerationZ());
+  accZ_ma=get_ma(acc_maZ);
+  //accZ_ma=convertRawAcc(BMI160.getAccelerationZ());
 
 
-  xy_acc=get_general_acc(accX_ma,accY_ma);
+  xy_acc=get_xy_magn(accX_ma,accY_ma);
 
   update_arr(rot_maX, (convertRawGyro(BMI160.getRotationX())) );
   rotX_ma=get_ma(rot_maX);
@@ -165,24 +167,41 @@ void loop() {
   rotZ_ma=get_ma(rot_maZ);
 
   //if val is not from noise and is actual movement, then add to vel
+  //idea doens't really work atm
   if(accX_ma>1 || accX_ma<-1){
     velocityX+=accX_ma;
     }
   if(accY_ma>1 || accY_ma<-1){
       velocityY+=accY_ma;
     }
+
+    //if deccelerating, and not noise brakes on
+    if(xy_acc<-1){
+      if((blink_left==false)&& (blink_right==false)){
+        digitalWrite(led_left_pin,HIGH);
+        digitalWrite(led_right_pin,HIGH);
+      }
+      //if blinking, continue to blink but turn other led on as well
+      else if(blink_left)
+        digitalWrite(led_right_pin, HIGH);
+      else if(blink_right)
+        digitalWrite(led_left_pin,HIGH);
+      //hold brake light for a bit afterwards(replace with timer eventually)
+      delay(200);
+      
+    }
  
 
 //below if statement doesn't ever return true even if sensor is moved quickly.
 //(BMI160.motionDetected(X_AXIS,POSITIVE)) || (BMI160.motionDetected(Y_AXIS,POSITIVE))
 //print acceleration values
-  if(false){
+  if(true){
   Serial.print("acc:\t");
   Serial.print(accX_ma);
   Serial.print("\t");
   Serial.print(accY_ma);
   Serial.print("\t");
-  Serial.print(accZ_ma);
+  Serial.print(xy_acc);
   
   Serial.println();
   }
@@ -205,7 +224,15 @@ void loop() {
     Serial.print((convertRawGyro(BMI160.getRotationZ())));
     Serial.print("\t");
   }
-  delay(300);
+  //print button val stuff
+  if(false){
+    Serial.print("button val(R L): \t");
+    Serial.print(btn_right_val);
+    Serial.print("\t");
+    Serial.print(btn_left_val);
+    Serial.println();
+  }
+  delay(150);
 }
 
 float convertRawGyro(int gRaw) {
@@ -224,7 +251,9 @@ float convertRawGyro(int gRaw) {
  * +/- 16g          | 1024 LSB/mg
  * */
 float convertRawAcc(int aRaw){
-  return (aRaw*9.82)/128;
+  //maybe test that this is m/s^2 by using video and tracking sensor similar to physics lab, and compare results
+  //9.82 for grav, 1000 for mg, 8192 for +-2g.
+  return (aRaw*9.82*(10))/8192;
 }
 
 int get_ma(int* arr){
@@ -247,7 +276,7 @@ void update_arr(int* arr, int new_val){
   arr[avg_arr_size-1]=new_val;
 }
 
-int get_general_acc(int x, int y){
+int get_xy_magn(int x, int y){
 
   int result=0;
  
