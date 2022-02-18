a4cb5798dd57625bc6948474fd6956167344e2d0
tyro <jomomojo55@gmail.com>
2022-02-18 12:03:24 -0800

    block checkoff 2 code. accel data read, converted and used to control led.

--- src/main.cpp
+++ src/main.cpp
@@ -28,33 +28,29 @@ int get_ma(int* arr);
 void update_arr(int* arr,int new_val);
 //
 bool timer_funct();
+bool toggle_handler();
 
 
 auto timer = timer_create_default();
+auto toggle_led_timer=timer_create_default();
+
 
 /** Global Vars **/
-//defines the arr size of the moving avg for acc and rot if needed
-const int avg_arr_size=5;
+//
+int toggle_rate=1000; //start out at 1 second toggle rate 
 //used to keep track of time similiar to timer
 int long last_millis=0;
 int long curr_millis=0;
-//accel info
-float accel_sum,prev_accel_sum=0;
+//speed(only in x direction atm) info. used to sum the accelerometer readings over the period of 1 sec
+float velocity,prev_velocity=0;
 
 
-//used to keep track of moving average for X acceleration
-int acc_maX[avg_arr_size]={0};
-int acc_maY[avg_arr_size]={0};
-int acc_maZ[avg_arr_size]={0};
-//moving average of each axis of rotation
-int rot_maX[avg_arr_size]={0};
-int rot_maY[avg_arr_size]={0};
-int rot_maZ[avg_arr_size]={0};
+
 //left push button signal(active low)
-int const btn_left_pin=11;
+int const btn_left_pin=3;
 bool btn_left_val=true;
 //right push button signal(active low)
-int const btn_right_pin=12;
+int const btn_right_pin=4;
 bool btn_right_val=true;
 //left led signal
 int const led_left_pin=5;
@@ -67,12 +63,10 @@ bool blink_right=false;
 //vars to attempt to keep track of general motion
 bool moving_forward=false;
 bool braking=false;
-
-//keep track of velocity by adding acceleration values above threshold
-//idea was to try and sum up acceleration points when motion happens, however doesn't work since valeus don't exactly
-//add to 0, so would be able to really tell if stopped or how fast person is going.
-int velocityX=0;
-int velocityY=0;
+//dispalcement aimed to keep track of total ditance traveled
+int displacement=0;
+//var to try and adjust for error at resting place
+const float err_adj=.06;
 
 /*
 Setup Description: Initialize Serial, set 2 leds as outuput and take pushbutton as input.
@@ -82,8 +76,8 @@ void setup() {
   Serial.begin(9600); // initialize Serial communication
   while (!Serial);    // wait for the serial port to open
   //set btn as input, leds as ouptu
-  pinMode(btn_left_pin, INPUT);
-  pinMode(btn_right_pin, INPUT);
+  pinMode(btn_left_pin, INPUT_PULLUP);
+  pinMode(btn_right_pin, INPUT_PULLUP);
   pinMode(led_left_pin, OUTPUT);
   pinMode(led_right_pin, OUTPUT);
 
@@ -108,10 +102,13 @@ void setup() {
  // BMI160.setAccelRate(32000);
   BMI160.interruptsEnabled(CURIE_IMU_MOTION);
   //BMI160.setDetectionThreshold(CURIE_IMU_MOTION,2);
+  BMI160.setFullScaleAccelRange(8);
+  BMI160.setAccelRate(5);
   Serial.println(BMI160.getFullScaleAccelRange());
   //Serial.println(BMI160.getDetectionThreshold(CURIE_IMU_MOTION));
   Serial.println("Initializing IMU device...done.");
   timer.every(1000,timer_funct);
+  toggle_led_timer.every(toggle_rate,toggle_handler);
 }
 
 /*
@@ -121,15 +118,18 @@ Turn on led that isn't blinking and let the led that is blinking continue to bli
 */
 void loop() {
   
-  float accX,accY,accX_ma,accY_ma,accZ_ma,xy_acc, rotX_ma, rotY_ma, rotZ_ma;
-  
+  float accX,accY;
   timer.tick();
+  //toggle_led_timer.tick();
  
 
   //read button values
   btn_left_val=digitalRead(btn_left_pin);
   btn_right_val=digitalRead(btn_right_pin);
-  
+
+
+/*****  Just stuff for turn signls and button, not needed for checkoff of sensor  *****/
+  /*
   //if right btn pressed(active low), blink right led
   if(btn_right_val==0){
     //flip blink val to turn toggle essentially
@@ -156,7 +156,9 @@ void loop() {
   else{ //default to just led on(half brightness, fog light brakes)
     analogWrite(led_left_pin, 125);
   }
+  */
 
+/*
   if(blink_right){
     analogWrite(led_right_pin,(led_right_val?255:0));
     led_right_val=(!led_right_val);
@@ -165,67 +167,59 @@ void loop() {
     digitalWrite(led_left_pin,(led_left_val?255:0));
     led_left_val=(!led_left_val);
   }
+  */
 
 
   //add new val to arr of ma for X Y and Z arrs
   //update_arr(acc_maX, (convertRawAcc(BMI160.getAccelerationX())) );
   accX=convertRawAcc(BMI160.getAccelerationX());
-  if(accX>.05 || accX<-.05){
-    Serial.print("adding \t");
-    Serial.println(accX);
-    
-    accel_sum+=accX;
-    //Serial.println(accel_sum);
+  if(accX>.10 || accX<-.10){
+    if(false){
+      Serial.print("adding \t");
+      Serial.println(accX);
+    }
+
+    velocity+=(accX+err_adj);
+    //Serial.println(velocity);
   }
-  //accX_ma=convertRawAcc(BMI160.getAccelerationX());
+  
 
 
- 
 
-    
- 
-
-//below if statement doesn't ever return true even if sensor is moved quickly.
-//(BMI160.motionDetected(X_AXIS,POSITIVE)) || (BMI160.motionDetected(Y_AXIS,POSITIVE))
-//print acceleration values
+//print values
   if(false){
     Serial.print("acc:\t");
     Serial.print(convertRawAcc(BMI160.getAccelerationX()));
     Serial.print("\t");
     //Serial.print(convertRawAcc(BMI160.getAccelerationY()));
-    Serial.println(accel_sum);
+    Serial.println(velocity);
     //Serial.print(xy_acc);
-    
     Serial.println();
   }
-  //print test velocity values
+
+  //print raw accel data
   if(false){
-    Serial.print("velocity: \t");
-    Serial.print(velocityX);
-    Serial.print("\t");
-    Serial.print(velocityX);
-    Serial.println();
+    Serial.println("acceleration data:");
+    Serial.print("X:\t");
+    Serial.println(BMI160.getAccelerationX());
+    Serial.print("Y:\t");
+    Serial.println(BMI160.getAccelerationY());
+    Serial.print("Z:\t");
+    Serial.println(BMI160.getAccelerationZ());
   }
-  //print gryo rotation values
+  //gyro raw data
   if(false){
-    //display tab-separated gyro x/y/z values
-    Serial.print("rotation:\t");
-    Serial.print((convertRawGyro(BMI160.getRotationX())));
-    Serial.print("\t");
-    Serial.print((convertRawGyro(BMI160.getRotationY())));
-    Serial.print("\t");
-    Serial.print((convertRawGyro(BMI160.getRotationZ())));
-    Serial.print("\t");
+    Serial.println("gyro data:");
+    Serial.print("X:\t");
+    Serial.println(BMI160.getRotationX());
+    Serial.print("Y:\t");
+    Serial.println(BMI160.getRotationY());
+    Serial.print("Z:\t");
+    Serial.println(BMI160.getRotationZ());
   }
-  //print button val stuff
-  if(false){
-    Serial.print("button val(R L): \t");
-    Serial.print(btn_right_val);
-    Serial.print("\t");
-    Serial.print(btn_left_val);
-    Serial.println();
-  }
-  delay(1);
+  
+  
+  delay(150);
 }
 
 float convertRawGyro(int gRaw) {
@@ -245,27 +239,7 @@ float convertRawGyro(int gRaw) {
  * */
 float convertRawAcc(int aRaw){
   
-  return (aRaw*9.81)/16384;
-}
-
-int get_ma(int* arr){
-  int avg=0;
-  //for loops run for 20 since we know size of arrs
-  for(int i=0; i<avg_arr_size; i++){
-    avg+=arr[i];
-  }
-  //divide to get average
-  avg/=avg_arr_size;
-
-  return avg;
-}
-
-void update_arr(int* arr, int new_val){
-  //first go from the beg of arr, replacing the prev  index with the next index val
-  for(int i=0; i<avg_arr_size-1; i++){ //should only need to go to 18 index since last is replaced with new_val
-    arr[i]=arr[i+1];
-  }
-  arr[avg_arr_size-1]=new_val;
+  return (aRaw*9.81)/4096;
 }
 
 int get_xy_magn(int x, int y){
@@ -282,11 +256,42 @@ int get_xy_magn(int x, int y){
 //function called when timer val reached
 //used to get sum of acceleration measurments, reset the sum over time period, and update value
 bool timer_funct(){
-  if(false)
+  displacement+=velocity;
+  prev_velocity=velocity;
+  //if velocity is decreasing, or standing still, "brakes" are on
+  if(velocity<0){
+    digitalWrite(led_left_pin,HIGH);
+    digitalWrite(led_right_pin,HIGH);
+  }
+  //else toggle leds to flash
+  else{
+    digitalWrite(led_left_pin,!digitalRead(led_left_pin));
+    digitalWrite(led_right_pin,!digitalRead(led_right_pin));
+  }
+  //just for printing and testing vals
+  if(true){
     Serial.print("summed accel: \t");
-    Serial.println(accel_sum);
-  prev_accel_sum=accel_sum;
-  accel_sum=0;
+    Serial.print(velocity);
+    Serial.print("\tDisplacement:\t");
+    Serial.println(displacement);
+  }
+  
+  velocity=0;
 //return true to conitnue counter
+  return true;
+}
+
+bool toggle_handler(){
+  Serial.println("in toggle handler");
+  toggle_rate+=10;
+  Serial.print("left led: \t");
+  Serial.println(led_left_val);
+  Serial.print("Rigth led: \t");
+  Serial.println(led_right_val);
+  digitalWrite(led_left_pin,led_left_val);
+  led_left_val=(!led_left_val);
+  digitalWrite(led_right_pin,led_right_val);
+  led_right_val=(!led_right_val);
+  //toggle_led_timer.every(toggle_rate,toggle_handler);
   return true;
 }
